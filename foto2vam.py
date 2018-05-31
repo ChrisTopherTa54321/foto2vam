a9d3b4ce02aca7bafb16dd15035bfb099790c4b4
Christopher Throwaway <ChristopherTA54321@gmail.com>
2018-05-31 18:49:42 -0500

    Foto2vam iterates over all models
    Foto2Vam now iterates over all .model files and generates using
    them. Also added a new json file for the original model, since it
    was created with the missing 'trimToAnimatable' bug. The new
    json works around this.

--- foto2vam.py
+++ foto2vam.py
@@ -1,6 +1,7 @@
 # Quickly thrown together root script to run Tools
 import argparse
 import os
+import glob
 import Tools.CreateTrainingEncodings as encodings
 import Tools.MakePrediction as predictor
 import Tools.MergeJson as mergeJson
@@ -16,31 +17,33 @@ def main( args ):
         pydevd.settrace(suspend=False)
 
     inputPath = args.inputPath
-    modelPath = args.modelPath
+    modelGlob = args.modelPath
     outputPath = args.outputPath
     defaultJsonPath = args.defaultJson
     mergedJsonPath = args.mergedOutputPath
-    jsonPath = os.path.splitext(modelPath)[0] + ".json"
 
-    print( "Processing images from {} and using model/json {}/{}".format(inputPath, modelPath, jsonPath))
+    print( "Processing images from {}".format(inputPath))
 
     print( "First running CreateTrainingEncodings tool")
     params = argparse.Namespace(inputPath=inputPath, filter="*.png,*.jpg", normalizeSize=150, normalize=True, numJitters=10, numThreads=4, pydev=False, recursive=True, debugPose = False)
     encodings.main( params )
 
-    print( "Running MakePredictions tool")
-    params = argparse.Namespace(modelFile=modelPath, inputDir=inputPath, pydev=False, outputDir=outputPath, multiDir=False, skipChance=0.0, recursive=True )
-    predictor.main(params)
+    for modelFile in glob.glob( modelGlob ):
+        jsonPath = os.path.splitext(modelFile)[0] + ".json"
+        print( "Processing encodings from {} and using model/json {}/{}".format(inputPath, modelFile, jsonPath))
+        print( "Running MakePredictions tool")
+        print( "With model {}".format(modelFile))
+        params = argparse.Namespace(modelFile=modelFile, inputDir=inputPath, pydev=False, outputDir=outputPath, multiDir=False, skipChance=0.0, recursive=True )
+        predictor.main(params)
     
-    print( "Running MergeJson tool" )
-    jsonData = json.loads( open(jsonPath).read() )
+        print( "Running MergeJson tool" )
+        jsonData = json.loads( open(jsonPath).read() )
+        # Run MergeTool using the inverted baseJson (copy all attributes except the ones trained on)
+        templateJson = jsonData["baseJson"]
     
-    # Run MergeTool using the inverted baseJson (copy all attributes except the ones trained on)
-    templateJson = jsonData["baseJson"]
-
-    params = None
-    params = argparse.Namespace(templateJson=templateJson, invertTemplate=True, toJsonDir=outputPath, filter="*.json", recursive=True, fromJson=defaultJsonPath, outputJsonDir=mergedJsonPath, pydev=False)
-    mergeJson.main(params)
+        params = None
+        params = argparse.Namespace(templateJson=templateJson, invertTemplate=True, toJsonDir=outputPath, filter="*.json", recursive=True, fromJson=defaultJsonPath, outputJsonDir=mergedJsonPath, pydev=False)
+        mergeJson.main(params)
 
 
 ###############################
@@ -49,7 +52,7 @@ def main( args ):
 def parseArgs():
     parser = argparse.ArgumentParser( description="Generate training data" )
     parser.add_argument('--inputPath', help="Directory containing images", default="Input")
-    parser.add_argument('--modelPath', help="Path to model", default=os.path.join("models", "foto2vam.model") )
+    parser.add_argument('--modelPath', help="Path to model, can include wildcard", default=os.path.join("models", "*.model") )
     parser.add_argument('--defaultJson', help="JSON file to copy base look from", default=os.path.join("Sample", "default.json") )
     parser.add_argument('--outputPath', help="Directory to store output", default="Output")
     parser.add_argument('--mergedOutputPath', help="Path to store output merged with defaultJson", default="Output_Merged")
